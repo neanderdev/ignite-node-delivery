180b2e1737b63a271f59a103ef6c9b911f8b6d48
neanderdev <neander.faria@hotmail.com>
2022-07-06 14:33:56 -0300

    Delivery search without deliveryman attached

---
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
@@ -0,0 +1,13 @@
+import { prismaClient } from "../../../../database/prismaClient";
+
+export class FindAllAvailableUseCase {
+  async execute() {
+    const deliveries = await prismaClient.deliveries.findMany({
+      where: {
+        end_at: null,
+      },
+    });
+
+    return deliveries;
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,9 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
-import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
 
 import { ensureAuthenticateClient } from "./modules/middlewares/ensureAuthenticateClient";
 
@@ -15,7 +16,9 @@ const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post("/client/authenticate/", authenticateClientController.handle);
 routes.post(
@@ -31,5 +34,6 @@ routes.post(
   ensureAuthenticateClient,
   createDeliveryController.handle
 );
+routes.get("/delivery/available/", findAllAvailableController.handle);
 
 export { routes };
